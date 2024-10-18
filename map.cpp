345048531321f77d39c2bdd9e5111729829891f6
metflex <106245122+metflekx@users.noreply.github.com>
2024-10-18 11:24:30 +0300

    Draws Spiral

--- map.cpp
+++ map.cpp
@@ -1,4 +1,5 @@
 #include "map.h"
+#include <math.h>
 
 /* Initialize map, Set width and height */
 Map::Map(int w, int h) {
@@ -11,5 +12,13 @@ Map::~Map() {}
 
 /* Draw the spiral background */
 void Map::draw_map(SDL_Renderer *renderer) {
-    SDL_RenderDrawLine(renderer, 10, 10, 100, 100);
+    double t, x_offset, y_offset;
+
+    // Get the spiral in the middle
+    x_offset = (double) width / 2.0;
+    y_offset = (double) height / 2.0;
+
+    for (t = 0.0; t < 100.0; t += 0.01)
+        SDL_RenderDrawPointF(renderer, 
+        x_offset + (t*cos(t)), y_offset + (t*sin(t)));
 }
