3bf15b69febb657e7f026679b214d690c040069c
Mahdi Habibi <em.habibi@hotmail.com>
2026-05-06 11:17:57 +0300

    main - Create window using the new road animation at main

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main() {
   uint32_t framestart;
   int frametime;
 
-  game->init("Lightning", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800,
-             600, false);
+  game->init("Road", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280,
+             800, false);
 
   while (game->is_running()) {
     // Get current timing in ms
